17d52b0e86eb6bd5d8c4c7496530b577ed136d0f
Phil Nachum <phil.nachum@addepar.com>
2015-07-17 13:58:13 -0700

    some cleanup and comments

--- public/js/app.js
+++ public/js/app.js
@@ -6,51 +6,59 @@ $(function() {
   })
 });
 
-var generateName = function() {
+function generateName() {
   do {
     var first = _.sample(firsts);
     var last = _.sample(lasts);
-    var subtitle = _.sample(subtitles);
     var franchise = first + last;
+    var subtitle = _.sample(subtitles);
+
+    // whether or not to include a subtitle
     var shouldSubtitle = _.sample([true, false]);
+    // whether or not to include a number
     var shouldNumber = _.sample([true, false]);
+  // realNames contains a blacklist of actual franchises. I don't want the
+  // generator to produce names of real properties.
   } while (_.contains(realNames, franchise));
 
   var output = franchise;
   if (shouldNumber) {
     var number = _.random(2, 5);
-    var shouldRoman = _.random(0, 2);
-    if (shouldRoman === 0) {
-      output += " " + convertToRoman(number);
-    } else {
-      output += " " + number;
+    var shouldRoman = _.sample([true, false]);
+    if (shouldRoman) {
+      number = convertToRoman(number);
     }
-
+      output += " " + number;
   }
+
   if (shouldSubtitle) {
     output += ": " + subtitle;
   }
   return output;
-};
+}
 
-var fillInName = function() {
+function fillInName() {
   var name = generateName();
   $('.name').html(name);
-};
+}
 
-var firsts = ["Battle", "Blood", "Over", "Sky", "Kill", "Dark", "Black", "War", "Shadow"];
-var lasts = ["cry", "field", "battle", "death", "dead", "borne", " Ops", "strike", "forge", "light", "fall"];
-var subtitles = ["Revelations", "Origins", "Awakening", "Apocalypse", "Reckoning", "Damnation", "Legends", "Heroes", "Vengeance", "Revenge"];
+// The building blocks of a modern video game name
+var firsts = ["Battle", "Blood", "Over", "Sky", "Kill", "Dark", "Black", "War",
+  "Shadow"];
+var lasts = ["cry", "field", "battle", "death", "dead", "borne", " Ops",
+  "strike", "forge", "light", "fall"];
+var subtitles = ["Revelations", "Origins", "Awakening", "Apocalypse",
+  "Reckoning", "Damnation", "Legends", "Heroes", "Vengeance", "Revenge"];
 
+// Somewhat amusing how large this list has to be
 var realNames = ["Battlecry", "Bloodborne", "Battleborne", "Battlefield",
   "Overstrike", "Black Ops", "Blacklight", "Skyfall", "Darkfall", "Shadowfall",
   "Skyforge", "Battleforge", "Battlestrike", "Bloodstrike", "Bloodforge",
   "Skyborne", "Warcry"];
 
-var convertToRoman = function(num) {
+// For the purposes of this code, num is between 2 and 5
+function convertToRoman(num) {
   switch (num) {
-    case 1:
-      return "I";
     case 2:
       return "II";
     case 3:
